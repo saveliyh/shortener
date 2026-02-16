1d99cb8d51175d0366b496bc815457e98d82174c
Saveliy Khlebnov <savel.khlebnov@yandex.ru>
2026-02-16 05:10:46 +0300

    more tests

--- internal/logic/logic_test.go
+++ internal/logic/logic_test.go
@@ -12,38 +12,38 @@ func Test_create_short_link(t *testing.T) {
 	}
 }
 
-func Test_unshorten_link_same_long_link(t *testing.T) {
+func Test_unshorten_link_same_input(t *testing.T) {
 	name := "unshorten_link test for same input"
 	storage := TestStorage{}
 	short_link := "short_link1"
 	long_link1, err := Unshorten_link(short_link, storage)
 	if err != nil {
-		t.Errorf("%s: error is not nil", name)
+		t.Errorf("%s: error is not nil. %s", name, err)
 	}
 	long_link2, err := Unshorten_link(short_link, storage)
 	if err != nil {
-		t.Errorf("%s: error is not nil", name)
+		t.Errorf("%s: error is not nil. %s", name, err)
 	}
 	if long_link1 != long_link2 {
-		t.Errorf("%s: long_link1 is not equal to long_link2", name)
+		t.Errorf("%s: got different long_link when retrieved", name)
 	}
 
 }
-func Test_unshorten_link_different_long_link(t *testing.T) {
+func Test_unshorten_link_different_input(t *testing.T) {
 	name := "unshorten_link test for different input"
 	storage := TestStorage{}
 	short_link1 := "short_link1"
 	long_link1, err := Unshorten_link(short_link1, storage)
 	if err != nil {
-		t.Errorf("%s: error is not nil", name)
+		t.Errorf("%s: error is not nil. %s", name, err)
 	}
 	short_link2 := "short_link2"
 	long_link2, err := Unshorten_link(short_link2, storage)
 	if err != nil {
-		t.Errorf("%s: error is not nil", name)
+		t.Errorf("%s: error is not nil. %s", name, err)
 	}
 	if long_link1 == long_link2 {
-		t.Errorf("%s: long_link1 is equal to long_link2", name)
+		t.Errorf("%s: got same long_link for different short_links", name)
 	}
 }
 
@@ -54,5 +54,37 @@ func Test_unshorten_link_wrong_input(t *testing.T) {
 	_, err := Unshorten_link(short_link, storage)
 	if err == nil {
 		t.Errorf("%s: Had to return \"not found error\"", name)
+	}
+}
+
+func Test_get_short_link_add_and_retrieve(t *testing.T) {
+	name := "get_short_link test for add and retrieve"
+	storage := TestStorage{}
+	long_link := "long_link3"
+	short_link1, err := Get_short_link(long_link, storage)
+	storage.test_store_in_db(short_link1)
+	if err != nil {
+		t.Errorf("%s: error is not nil. %s", name, err)
+	}
+	short_link2, err := Get_short_link(long_link, storage)
+	if err != nil {
+		t.Errorf("%s: error is not nil. %s", name, err)
+	}
+	println(short_link1, short_link2)
+	if short_link1 != short_link2 {
+		t.Errorf("%s: got new short_link when retrieved", name)
+	}
+}
+
+func Test_get_short_link_correct_link_length(t *testing.T) {
+	name := "get_short_link test for correct link length"
+	storage := TestStorage{}
+	long_link := "long_link"
+	short_link, err := Get_short_link(long_link, storage)
+	if err != nil {
+		t.Errorf("%s: error is not nil. %s", name, err)
+	}
+	if len(short_link) != LINK_LENGTH {
+		t.Errorf("%s: short_link length is not equal to %d", name, LINK_LENGTH)
 	}
 }
--- internal/logic/test_structs.go
+++ internal/logic/test_structs.go
@@ -5,17 +5,24 @@ import (
 	"url_shortener/internal/database"
 )
 
-type TestStorage struct{}
+type TestStorage struct {
+	added      bool
+	added_link string
+}
 
 func (t TestStorage) Connect_db() (database.Storage, error) {
 	return t, nil
 }
 
 func (t TestStorage) Check_long_link(long_link string) bool {
-	return long_link == "long_link1" || long_link == "long_link2"
+	return long_link == "long_link1" ||
+		long_link == "long_link2" ||
+		(t.added && long_link == "long_link3")
 }
 func (t TestStorage) Check_short_link(short_link string) bool {
-	return short_link == "short_link1" || short_link == "short_link2"
+	return short_link == "short_link1" ||
+		short_link == "short_link2" ||
+		(t.added && short_link == t.added_link)
 }
 func (t TestStorage) Get_short_link(long_link string) (string, error) {
 	if long_link == "long_link1" {
@@ -23,6 +30,9 @@ func (t TestStorage) Get_short_link(long_link string) (string, error) {
 	}
 	if long_link == "long_link2" {
 		return "short_link2", nil
+	}
+	if long_link == "long_link3" && t.added {
+		return t.added_link, nil
 	}
 	return "", errors.New("Long link not exist in database")
 }
@@ -33,6 +43,9 @@ func (t TestStorage) Get_long_link(short_link string) (string, error) {
 	if short_link == "short_link2" {
 		return "long_link2", nil
 	}
+	if short_link == t.added_link && t.added {
+		return "long_link3", nil
+	}
 	return "", errors.New("Short link not exist in database")
 }
 func (t TestStorage) Store_in_db(short_link string, long_link string) error {
@@ -40,4 +53,9 @@ func (t TestStorage) Store_in_db(short_link string, long_link string) error {
 		return errors.New("Short link already exist in database")
 	}
 	return nil
+}
+
+func (t *TestStorage) test_store_in_db(short_link string) {
+	t.added = true
+	t.added_link = short_link
 }
